bce31348b089fcac3fd507c55f7f177de2b9c064
Baawaah <son.tran.be@gmail.com>
2016-10-14 23:16:30 +0200

    few starting code

---
+++ HM1/main.m
@@ -0,0 +1,36 @@
+% Fluide and Transfert Mechanical 2
+%  by Philippe de Posson   ????-??-??
+%     Thanh-Son Tran       8116-12-00
+%% =======================================
+% Data
+% Nozzle  
+nozzle_de = 0.015;          % [m]
+nozzle_he  = nozzle_de / 2; % [m]
+
+% Chenal Duct
+duct_w = 0.050; % [m]
+duct_h = nozzle_h; % [m]
+
+% For this problematic
+T0 = 300; % [K]
+pa = 1;   % [atm] ~= [bar]
+%% =======================================
+% 3 Questions:
+% 1) Determine p0 so
+%    a/ sonic at throat and subsonic downstream
+%    b/ sonic at throat and supersonic up to a shock located at 70.0 mm
+%    c/ sonic at throat and supersonic up to a shock located at 120.0 mm
+% 2) Determine the mass flow rate Qm for each case
+% 3) Compute the flow condition p(x),p0(x) and M(x).
+%% =======================================
+% Question 1)
+
+%% =======================================
+% Question 2)
+
+%% =======================================
+% Question 3)
+
+%% =======================================
+
+
